1ea8d1535e64d3d28663516c27bc30368f0b7eea
Jason Woo <jaswoo73@gmail.com>
2024-01-15 21:41:56 +0800

    implement ecommerce functionality

--- src/app/_providers/Theme/InitTheme/index.tsx
+++ src/app/_providers/Theme/InitTheme/index.tsx
@@ -7,7 +7,8 @@ export const InitTheme: React.FC = () => {
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
     <Script
       id="theme-script"
-      strategy="beforeInteractive"
+      // comment out the line below to avoid unhandled runtime error: ChunkLoadError
+      // strategy="beforeInteractive"
       dangerouslySetInnerHTML={{
         __html: `
   (function () {
